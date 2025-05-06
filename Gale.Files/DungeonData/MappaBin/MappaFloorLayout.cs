351094f071aa58c0678ed726b7d84e93d42ee153
Jordan Marine <marine.jordan64@gmail.com>
2025-05-06 15:55:40 -0500

    Updated MappaFloorLayout to use more sane logic for initialization. This removes MappaBinReader.

--- Gale.Files/DungeonData/MappaBin/MappaFloorLayout.cs
+++ Gale.Files/DungeonData/MappaBin/MappaFloorLayout.cs
@@ -1,87 +1,29 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
 namespace Gale.Files.DungeonData.MappaBin;
 
 /// <summary>
 /// Structure used to store mappa floor layout data.
 /// </summary>
-/// <param name="structure">The structure of the floor.</param>
-/// <param name="roomDensity">The density of the rooms on the floor.</param>
-/// <param name="tilesetId">The tileset that will be used for this floor.</param>
-/// <param name="musicId">The music that will play on the floor.</param>
-/// <param name="weather">Type of weather that will appear.</param>
-/// <param name="floorConnectivity"></param>
-/// <param name="initialEnemyDensity">Amount of enemies that spawn on the floor when first loaded.</param>
-/// <param name="kecleonShopChance">The chance this floor will have a shop.</param>
-/// <param name="monsterHouseChance">The chance this floor will have a monster house.</param>
-/// <param name="unusedChance">Unused unless using the relevant patch.</param>
-/// <param name="stickyItemChance">Chance that a spawned item will be sticky.</param>
-/// <param name="deadEnds">True if dead ends should be generated, otherwise false.</param>
-/// <param name="secondaryTerrain">If greater than 0, rooms on the floor will spawn with secondary terrain (water, lava, void).</param>
-/// <param name="terrainSettings">Bitflag which stores extra terrain settings.</param>
-/// <param name="unkE">UNK: This seems to be unused.</param>
-/// <param name="itemDensity">Determines the amount of items spawned on a floor.</param>
-/// <param name="trapDensity">Determines the amount of traps spawned on a floor.</param>
-/// <param name="floorNumber">The number of the floor.</param>
-/// <param name="fixedFloorId">Represents the ID of a fixed floor or floor part.</param>
-/// <param name="extraHallwayDensity">Adds extra (mostly confusing) hallways to the floor.</param>
-/// <param name="buriedItemDensity">Determines the amount of buried items spawned on a floor.</param>
-/// <param name="waterDensity">Determines the amount of water spawned on the floor.</param>
-/// <param name="darknessLevel">Determines how far that can be seen in hallways.</param>
-/// <param name="maxCoinAmount">Max amount of coins to be spawned in each 'pile'.</param>
-/// <param name="kecleonShopItemPositions">Determines the spawn position of shop items.</param>
-/// <param name="emptyMonsterHouseChance">Chance that a monster house will spawn with no items.</param>
-/// <param name="hiddenStairType">Determines the type of hidden stair spawned.</param>
-/// <param name="hiddenStairsSpawnChance">Chance that a hidden stair will spawn on the floor.</param>
-/// <param name="enemyIq">Amount of IQ enemies have on this floor.</param>
-/// <param name="iqBoosterBoost">Amount of IQ gained from the IQ Booster on this floor, or none if less than or equal to 0.</param>
-public readonly struct MappaFloorLayout(
-    FloorStructure structure,
-    sbyte roomDensity,
-    byte tilesetId,
-    byte musicId,
-    FloorWeather weather,
-    byte floorConnectivity,
-    sbyte initialEnemyDensity,
-    byte kecleonShopChance,
-    byte monsterHouseChance,
-    byte unusedChance,
-    byte stickyItemChance,
-    bool deadEnds,
-    byte secondaryTerrain,
-    MappaFloorTerrainSettings terrainSettings,
-    bool unkE,
-    byte itemDensity,
-    byte trapDensity,
-    byte floorNumber,
-    byte fixedFloorId,
-    byte extraHallwayDensity,
-    byte buriedItemDensity,
-    byte waterDensity,
-    byte darknessLevel,
-    int maxCoinAmount,
-    byte kecleonShopItemPositions,
-    byte emptyMonsterHouseChance,
-    HiddenStairType hiddenStairType,
-    byte hiddenStairsSpawnChance,
-    ushort enemyIq,
-    short iqBoosterBoost
-)
+public readonly struct MappaFloorLayout : IEquatable<MappaFloorLayout>
 {
     /// <summary>
     /// The structure of the floor.
     /// </summary>
-    public FloorStructure Structure { get; } = structure;
+    public FloorStructure Structure { get; }
 
     /// <summary>
     /// The density of the rooms on the floor.
     /// The game randomly adds a number between 0 
     /// and 2 to obtain the final value.
     /// </summary>
-    public sbyte RoomDensity { get; } = roomDensity;
+    public sbyte RoomDensity { get; }
 
     /// <summary>
     /// The ID of the tileset to be used for this floor.
     /// </summary>
-    public byte TilesetId { get; } = tilesetId;
+    public byte TilesetId { get; }
 
     /// <summary>
     /// The ID of the music to play, based on a seperate table 
@@ -98,12 +40,12 @@ public readonly struct MappaFloorLayout(
     /// The value of the chosen entry is the ID of the song that will play in the floor. This is used in 
     /// floors where the music isn't fixed.
     /// </remarks>
-    public byte MusicId { get; } = musicId;
+    public byte MusicId { get; }
 
     /// <summary>
     /// The weather that will occur on this floor.
     /// </summary>
-    public FloorWeather Weather { get; } = weather;
+    public FloorWeather Weather { get; }
 
     /// <summary>
     /// Amount of connections between cells that will be generated
@@ -114,22 +56,22 @@ public readonly struct MappaFloorLayout(
     /// Minimum 1, if 0 then the generation fails and falls back
     /// to <see cref="FloorStructure.SingleRoomMonsterHouse"/>.
     /// </remarks>
-    public byte FloorConnectivity { get; } = floorConnectivity;
+    public byte FloorConnectivity { get; }
 
     /// <summary>
     /// Initial enemy density.
     /// </summary>
-    public sbyte InitialEnemyDensity { get; } = initialEnemyDensity;
+    public sbyte InitialEnemyDensity { get; }
 
     /// <summary>
     /// Chance that a shop will spawn on this floor.
     /// </summary>
-    public byte KecleonShopChance { get; } = kecleonShopChance;
+    public byte KecleonShopChance { get; }
 
     /// <summary>
     /// Chance that a monster house will spawn on this floor.
     /// </summary>
-    public byte MonsterHouseChance { get; } = monsterHouseChance;
+    public byte MonsterHouseChance { get; }
 
     /// <summary>
     /// Varies between 0 and 1 on every floor, but it's not a flag.
@@ -141,55 +83,55 @@ public readonly struct MappaFloorLayout(
     /// If the relevant patch is applied, this is the chance that a random 
     /// room will be a maze of wall tiles.
     /// </remarks>
-    public byte UnusedChance { get; } = unusedChance;
+    public byte UnusedChance { get; }
 
     /// <summary>
     /// Chance that an item will spawn sticky.
     /// </summary>
-    public byte StickyItemChance { get; } = stickyItemChance;
+    public byte StickyItemChance { get; }
 
     /// <summary>
     /// True if dead ends are allowed on the floor, otherwise false.
     /// </summary>
-    public bool DeadEnds { get; } = deadEnds;
+    public bool DeadEnds { get; }
 
     /// <summary>
     /// If greater than 0, rooms on the floor will spawn with secondary
     /// terrain (water, lava, void).
     /// </summary>
-    public byte SecondaryTerrain { get; } = secondaryTerrain;
+    public byte SecondaryTerrain { get; }
 
     /// <summary>
     /// Set of bitflags used to control extra terrain settings.
     /// </summary>
-    public MappaFloorTerrainSettings TerrainSettings { get; } = terrainSettings;
+    public MappaFloorTerrainSettings TerrainSettings { get; }
 
     /// <summary>
     /// UNK: This seems to be unused.
     /// </summary>
-    public bool UnkE { get; } = unkE;
+    public bool UnkE { get; }
 
     /// <summary>
     /// The density of items spawned on the floor map.
     /// </summary>
-    public byte ItemDensity { get; } = itemDensity;
+    public byte ItemDensity { get; }
 
     /// <summary>
     /// Density of traps on the floor, calculated randomly between
     /// <see cref="TrapDensity"/> / 2 and <see cref="TrapDensity"/>
     /// </summary>
-    public byte TrapDensity { get; } = trapDensity;
+    public byte TrapDensity { get; }
 
     /// <summary>
     /// The number of this floor.
     /// </summary>
-    public byte FloorNumber { get; } = floorNumber;
+    public byte FloorNumber { get; }
 
     /// <summary>
     /// ID of the fixed floor or floor fragment of this floor.
     /// This includes key chambers, boss battle rooms, final rooms, etc.
     /// </summary>
-    public byte FixedFloorId { get; } = fixedFloorId;
+    public byte FixedFloorId { get; }
 
     /// <summary>
     /// Used to generate additional hallways in the map (those "donuts" 
@@ -197,17 +139,17 @@ public readonly struct MappaFloorLayout(
     /// room exits connected to the same room, those dead ends that 
     /// come out of a room,  make a couple of twists and also lead to nowhere).
     /// </summary>
-    public byte ExtraHallwayDensity { get; } = extraHallwayDensity;
+    public byte ExtraHallwayDensity { get; }
 
     /// <summary>
     /// Density of items buried on the floor map.
     /// </summary>
-    public byte BuriedItemDensity { get; } = buriedItemDensity;
+    public byte BuriedItemDensity { get; }
 
     /// <summary>
     /// Density of water tiles on the floor.
     /// </summary>
-    public byte WaterDensity { get; } = waterDensity;
+    public byte WaterDensity { get; }
 
     /// <summary>
     /// Represents the amount of tiles of vision in hallways, or full vision if 0.
@@ -215,7 +157,7 @@ public readonly struct MappaFloorLayout(
     /// <remarks>
     /// Dungeons normally use 0, 1, or 2, but higher values will work.
     /// </remarks>
-    public byte DarknessLevel { get; } = darknessLevel;
+    public byte DarknessLevel { get; }
 
     /// <summary>
     /// Represents the max coin count spawned in piles.
@@ -225,32 +167,32 @@ public readonly struct MappaFloorLayout(
     /// is to be multiplied by 5 when read and divided by 5 when written 
     /// to be accurately represented.
     /// </remarks>
-    public int MaxCoinAmount { get; } = maxCoinAmount;
+    public int MaxCoinAmount { get; }
 
     /// <summary>
     /// Controls where in a shop items will be placed.
     /// </summary>
-    public byte KecleonShopItemPositions { get; } = kecleonShopItemPositions;
+    public byte KecleonShopItemPositions { get; }
 
     /// <summary>
     /// Chance that a spawned monster house will have no items.
     /// </summary>
-    public byte EmptyMonsterHouseChance { get; } = emptyMonsterHouseChance;
+    public byte EmptyMonsterHouseChance { get; }
 
     /// <summary>
     /// Type of hidden stairs that will be spawned.
     /// </summary>
-    public HiddenStairType HiddenStairType { get; } = hiddenStairType;
+    public HiddenStairType HiddenStairType { get; }
 
     /// <summary>
     /// Chance that a hidden stair will spawn on the floor.
     /// </summary>
-    public byte HiddenStairsSpawnChance { get; } = hiddenStairsSpawnChance;
+    public byte HiddenStairsSpawnChance { get; }
 
     /// <summary>
     /// Amount of IQ that enemies will have on the floor.
     /// </summary>
-    public ushort EnemyIq { get; } = enemyIq;
+    public ushort EnemyIq { get; }
 
     /// <summary>
     /// Amount the IQ is increased by the IQ booster when entering a floor.
@@ -258,10 +200,82 @@ public readonly struct MappaFloorLayout(
     /// <remarks>
     /// Values less than or equal to 0 cause IQ to not be affected.
     /// </remarks>
-    public short IqBoosterBoost { get; } = iqBoosterBoost;
+    public short IqBoosterBoost { get; }
 
     /// <summary>
-    /// 
+    /// Hash of the binary data this instance contains. Used for comparison.
+    /// </summary>
+    private readonly string md5;
+
+    private MappaFloorLayout(
+        FloorStructure structure,
+        sbyte roomDensity,
+        byte tilesetId,
+        byte musicId,
+        FloorWeather weather,
+        byte floorConnectivity,
+        sbyte initialEnemyDensity,
+        byte kecleonShopChance,
+        byte monsterHouseChance,
+        byte unusedChance,
+        byte stickyItemChance,
+        bool deadEnds,
+        byte secondaryTerrain,
+        MappaFloorTerrainSettings terrainSettings,
+        bool unkE,
+        byte itemDensity,
+        byte trapDensity,
+        byte floorNumber,
+        byte fixedFloorId,
+        byte extraHallwayDensity,
+        byte buriedItemDensity,
+        byte waterDensity,
+        byte darknessLevel,
+        int maxCoinAmount,
+        byte kecleonShopItemPositions,
+        byte emptyMonsterHouseChance,
+        HiddenStairType hiddenStairType,
+        byte hiddenStairsSpawnChance,
+        ushort enemyIq,
+        short iqBoosterBoost,
+        string md5Hash
+    )
+    {
+        this.Structure = structure;
+        this.RoomDensity = roomDensity;
+        this.TilesetId = tilesetId;
+        this.MusicId = musicId;
+        this.Weather = weather;
+        this.FloorConnectivity = floorConnectivity;
+        this.InitialEnemyDensity = initialEnemyDensity;
+        this.KecleonShopChance = kecleonShopChance;
+        this.MonsterHouseChance = monsterHouseChance;
+        this.UnusedChance = unusedChance;
+        this.StickyItemChance = stickyItemChance;
+        this.DeadEnds = deadEnds;
+        this.SecondaryTerrain = secondaryTerrain;
+        this.TerrainSettings = terrainSettings;
+        this.UnkE = unkE;
+        this.ItemDensity = itemDensity;
+        this.TrapDensity = trapDensity;
+        this.FloorNumber = floorNumber;
+        this.FixedFloorId = fixedFloorId;
+        this.ExtraHallwayDensity = extraHallwayDensity;
+        this.BuriedItemDensity = buriedItemDensity;
+        this.WaterDensity = waterDensity;
+        this.DarknessLevel = darknessLevel;
+        this.MaxCoinAmount = maxCoinAmount;
+        this.KecleonShopItemPositions = kecleonShopItemPositions;
+        this.EmptyMonsterHouseChance = emptyMonsterHouseChance;
+        this.HiddenStairType = hiddenStairType;
+        this.HiddenStairsSpawnChance = hiddenStairsSpawnChance;
+        this.EnemyIq = enemyIq;
+        this.IqBoosterBoost = iqBoosterBoost;
+        this.md5 = md5Hash;
+    }
+
+    /// <summary>
+    /// Converts the structure back into mappa binary.
     /// </summary>
     /// <returns></returns>
     public byte[] ToMappa()
@@ -307,13 +321,18 @@ public readonly struct MappaFloorLayout(
     /// <summary>
     /// Loads floor layout from mappa binary.
     /// </summary>
-    /// <param name="reader">Mappa binary reader.</param>
-    /// <param name="offset">Offset of the floor layout data within the mappa file.</param>
+    /// <param name="mappaFloorData">32-length byte array containing the mappa floor data.</param>
     /// <returns></returns>
-    public static MappaFloorLayout FromMappa(MappaBinReader reader, int offset)
+    public static MappaFloorLayout FromMappa(byte[] mappaFloorData)
     {
-        BinaryReader br = reader.Read;
-        br.BaseStream.Seek(offset, SeekOrigin.Begin);
+        if (mappaFloorData.Length != 32)
+        {
+            throw new InvalidDataException(
+                "The mappa floor binary data must be exactly 32 bytes in length."
+            );
+        }
+
+        using BinaryReader br = new(new MemoryStream(mappaFloorData));
 
         return new MappaFloorLayout(
             structure: (FloorStructure)br.ReadByte(),
@@ -345,9 +364,59 @@ public readonly struct MappaFloorLayout(
             hiddenStairType: (HiddenStairType)br.ReadByte(),
             hiddenStairsSpawnChance: br.ReadByte(),
             enemyIq: br.ReadUInt16(),
-            iqBoosterBoost: br.ReadInt16()
+            iqBoosterBoost: br.ReadInt16(),
+            md5Hash: Convert.ToBase64String(MD5.HashData(mappaFloorData))
         );
     }
 
-    // TODO: implement Equals and associated operators, probably using a hash of the binary data.
+    /// <summary>
+    /// Determines if two instances of <see cref="MappaFloorLayout"/> are equal.
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns>true if the current object is equal to the other parameter; otherwise, false.</returns>
+    public override bool Equals([NotNullWhen(true)] object obj)
+    {
+        if (obj is MappaFloorLayout other)
+        {
+            return ((IEquatable<MappaFloorLayout>)this).Equals(other);
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Returns the hashcode for this instance.
+    /// </summary>
+    /// <returns>A 32-bit signed integer hash code.</returns>
+    public override int GetHashCode()
+    {
+        return this.md5.GetHashCode();
+    }
+
+    /// <summary>
+    /// Determines if two instances of <see cref="MappaFloorLayout"/> are equal.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns>true if both objects are equal; otherwise, false.</returns>
+    public static bool operator ==(MappaFloorLayout left, MappaFloorLayout right)
+    {
+        return left.Equals(right);
+    }
+
+    /// <summary>
+    /// Determines if two instances of <see cref="MappaFloorLayout"/> are not equal.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns>true if both objects are not equal; otherwise, false.</returns>
+    public static bool operator !=(MappaFloorLayout left, MappaFloorLayout right)
+    {
+        return !(left == right);
+    }
+
+    bool IEquatable<MappaFloorLayout>.Equals(MappaFloorLayout other)
+    {
+        return this.md5 == other.md5;
+    }
 }
